71afe6d196007cdb85c62410b42ef8ced9bda528
Sravya2007 <sravyamarepally@outlook.com>
2020-07-13 17:28:29 +0530

    NEW CAR RACING GAME

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1", carImg1);
     car2 = createSprite(300,200);
+    car2.addImage("car2", carImg2);
     car3 = createSprite(500,200);
+    car3.addImage("car3", carImg3);
     car4 = createSprite(700,200);
+    car4.addImage("car4", carImg4);
     cars = [car1, car2, car3, car4];
   }
 
@@ -44,11 +48,14 @@ class Game {
     if(allPlayers !== undefined){
       //var display_position = 100;
       
+    background(groundImg);
+    image(trackImg, 0, -displayHeight*4, displayWidth, displayHeight*5);
+
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 250;
       var y;
 
       for(var plr in allPlayers){
@@ -56,7 +63,7 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 300;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
@@ -68,8 +75,6 @@ class Game {
           camera.position.y = cars[index-1].y
         }
        
-        //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
 
     }
@@ -79,6 +84,15 @@ class Game {
       player.update();
     }
 
+    if(player.distance > 3860) {
+      gameState = 2;
+    }
+
     drawSprites();
+  }
+
+  end() {
+    console.log("Game ended");
+    game.update(2);
   }
 }
